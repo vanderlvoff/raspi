3ff7d153d9c458b641464db388b2cb3085e61bf9
リヴォフ  ユーリ <vanderlvov@gmail.com>
2019-10-03 19:15:22 +0900

    Update relay.py
    Actualized RELAIS_1_GPIO with the blog info

--- relay.py
+++ relay.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
  
-RELAIS_1_GPIO = 4
+RELAIS_1_GPIO = 24
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
 GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
