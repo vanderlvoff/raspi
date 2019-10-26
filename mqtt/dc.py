5e8727e73d2861ca2f1336dd11e4a31c644412d5
Yury Lvov <vanderlvov@gmail.com>
2019-10-26 14:26:11 -0400

    Small adjustment for the certain chassis

--- mqtt/dc.py
+++ mqtt/dc.py
@@ -8,6 +8,7 @@ import time
 import json
 
 class DC:
+    LE_MAX = 0.80
     i2c = busio.I2C(SCL, SDA)
     # Create a simple PCA9685 class instance.
     pca = PCA9685(i2c, address=0x40)
@@ -19,7 +20,7 @@ class DC:
 
     def forward(self):
         self.motor_hr.throttle = 1
-        self.motor_hl.throttle = 1
+        self.motor_hl.throttle = self.LE_MAX
 
     def stop(self):
         self.motor_hr.throttle = 0
@@ -27,12 +28,12 @@ class DC:
     
     def right(self):
         self.motor_hr.throttle = -1
-        self.motor_hl.throttle = 1
+        self.motor_hl.throttle = self.LE_MAX
 
     def left(self):
         self.motor_hr.throttle = 1
-        self.motor_hl.throttle = -1
+        self.motor_hl.throttle = self.LE_MAX * -1
     
     def back(self):
         self.motor_hr.throttle = -1
-        self.motor_hl.throttle = -1
+        self.motor_hl.throttle = self.LE_MAX * -1
