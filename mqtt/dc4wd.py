db020946d1fc2fcf59d3602b2f8fe7543b5c423a
Yury Lvov <vanderlvov@gmail.com>
2021-08-13 13:29:15 +0900

    4wd rover: WIP dc module

--- mqtt/dc4wd.py
+++ mqtt/dc4wd.py
@@ -1,7 +1,6 @@
 # Import the PCA9685 module. Available in the bundle and here:
 # https://github.com/adafruit/Adafruit_CircuitPython_PCA9685
 from adafruit_pca9685 import PCA9685
-from adafruit_motor import motor
 from board import SCL, SDA
 import busio
 import time
@@ -10,41 +9,158 @@ import json
 class DC4WD:
     i2c = busio.I2C(SCL, SDA)
     # Create a simple PCA9685 class instance.
-    pca = PCA9685(i2c)
-    motor_rl = motor.DCMotor(pca.channels[10], pca.channels[9])
-    motor_rr = motor.DCMotor(pca.channels[7], pca.channels[8])
-    motor_hl = motor.DCMotor(pca.channels[2], pca.channels[1])
-    motor_hr = motor.DCMotor(pca.channels[3], pca.channels[4])
+    pca = PCA9685(i2c, address=0x40)
+    speed = 64000
     
     def __init__(self):
-        self.pca.frequency = 100
-
+        self.pca.frequency = 1500
+        
     def forward(self):
-        self.motor_rr.throttle = 1
-        self.motor_hr.throttle = 1
-        self.motor_rl.throttle = 1
-        self.motor_hl.throttle = 1
+        self.pca.channels[12].duty_cycle = 0xffff
+        self.pca.channels[13].duty_cycle = 0
+        self.pca.channels[14].duty_cycle = self.speed
+        
+        #left - rear
+        self.pca.channels[2].duty_cycle = 0xffff
+        self.pca.channels[1].duty_cycle = 0
+        self.pca.channels[0].duty_cycle = self.speed
+        
+        #right - rear
+        self.pca.channels[6].duty_cycle = 0xffff
+        self.pca.channels[5].duty_cycle = 0
+        self.pca.channels[7].duty_cycle = self.speed
+        
+        #right - head
+        self.pca.channels[9].duty_cycle = 0xffff
+        self.pca.channels[10].duty_cycle = 0
+        self.pca.channels[8].duty_cycle = self.speed
 
     def stop(self):
-        self.motor_rr.throttle = 0
-        self.motor_hr.throttle = 0
-        self.motor_rl.throttle = 0
-        self.motor_hl.throttle = 0
+        self.pca.channels[12].duty_cycle = 0
+        self.pca.channels[13].duty_cycle = 0
+        
+        #left - rear
+        self.pca.channels[2].duty_cycle = 0
+        self.pca.channels[1].duty_cycle = 0
+
+        
+        #right - rear
+        self.pca.channels[6].duty_cycle = 0
+        self.pca.channels[5].duty_cycle = 0
+                
+        #right - head
+        self.pca.channels[9].duty_cycle = 0
+        self.pca.channels[10].duty_cycle = 0
+
     
     def right(self):
-        self.motor_rr.throttle = -1
-        self.motor_hr.throttle = -1
-        self.motor_rl.throttle = 1
-        self.motor_hl.throttle = 1
+        #left - head
+        self.pca.channels[12].duty_cycle = 0xffff
+        self.pca.channels[13].duty_cycle = 0
+        self.pca.channels[14].duty_cycle = self.speed
+        
+        #left - rear
+        self.pca.channels[2].duty_cycle = 0xffff
+        self.pca.channels[1].duty_cycle = 0
+        self.pca.channels[0].duty_cycle = self.speed
+        
+        #right - rear
+        self.pca.channels[6].duty_cycle = 0
+        self.pca.channels[5].duty_cycle = 0xffff
+        self.pca.channels[7].duty_cycle = self.speed
+        
+        #right - head
+        self.pca.channels[9].duty_cycle = 0
+        self.pca.channels[10].duty_cycle = 0xffff
+        self.pca.channels[8].duty_cycle = self.speed
+        
+        
+    def move_right(self):
+        #left - head
+        self.pca.channels[12].duty_cycle = 0xffff
+        self.pca.channels[13].duty_cycle = 0
+        self.pca.channels[14].duty_cycle = self.speed
+        
+        #left - rear
+        self.pca.channels[2].duty_cycle = 0
+        self.pca.channels[1].duty_cycle = 0xffff
+        self.pca.channels[0].duty_cycle = self.speed
+        
+        #right - rear
+        self.pca.channels[6].duty_cycle = 0xffff
+        self.pca.channels[5].duty_cycle = 0
+        self.pca.channels[7].duty_cycle = self.speed
+        
+        #right - head
+        self.pca.channels[9].duty_cycle = 0
+        self.pca.channels[10].duty_cycle = 0xffff
+        self.pca.channels[8].duty_cycle = self.speed
+        
+    def move_left(self):
+        
+        #left - head
+        self.pca.channels[12].duty_cycle = 0
+        self.pca.channels[13].duty_cycle = 0xffff
+        self.pca.channels[14].duty_cycle = self.speed
+        
+        #left - rear
+        self.pca.channels[2].duty_cycle = 0xffff
+        self.pca.channels[1].duty_cycle = 0
+        self.pca.channels[0].duty_cycle = self.speed
+        
+        #right - rear
+        self.pca.channels[6].duty_cycle = 0
+        self.pca.channels[5].duty_cycle = 0xffff
+        self.pca.channels[7].duty_cycle = self.speed
+        
+        #right - head
+        self.pca.channels[9].duty_cycle = 0xffff
+        self.pca.channels[10].duty_cycle = 0
+        self.pca.channels[8].duty_cycle = self.speed    
+        
 
     def left(self):
-        self.motor_rr.throttle = 1
-        self.motor_hr.throttle = 1
-        self.motor_rl.throttle = -1
-        self.motor_hl.throttle = -1
+        #left - head
+        self.pca.channels[12].duty_cycle = 0
+        self.pca.channels[13].duty_cycle = 0xffff
+        self.pca.channels[14].duty_cycle = self.speed
+        
+        #left - rear
+        self.pca.channels[2].duty_cycle = 0
+        self.pca.channels[1].duty_cycle = 0xffff
+        self.pca.channels[0].duty_cycle = self.speed
+        
+        #right - rear
+        self.pca.channels[6].duty_cycle = 0xffff
+        self.pca.channels[5].duty_cycle = 0
+        self.pca.channels[7].duty_cycle = self.speed
+        
+        #right - head
+        self.pca.channels[9].duty_cycle = 0xffff
+        self.pca.channels[10].duty_cycle = 0
+        self.pca.channels[8].duty_cycle = self.speed
     
-    def back():
-        self.motor_rr.throttle = -1
-        self.motor_hr.throttle = -1
-        self.motor_rl.throttle = -1
-        self.motor_hl.throttle = -1
+    def back(self):
+        #left - head
+        self.pca.channels[12].duty_cycle = 0
+        self.pca.channels[13].duty_cycle = 0xffff
+        self.pca.channels[14].duty_cycle = self.speed
+        
+        #left - rear
+        self.pca.channels[2].duty_cycle = 0
+        self.pca.channels[1].duty_cycle = 0xffff
+        self.pca.channels[0].duty_cycle = self.speed
+        
+        #right - rear
+        self.pca.channels[6].duty_cycle = 0
+        self.pca.channels[5].duty_cycle = 0xffff
+        self.pca.channels[7].duty_cycle = self.speed
+        
+        #right - head
+        self.pca.channels[9].duty_cycle = 0
+        self.pca.channels[10].duty_cycle = 0xffff
+        self.pca.channels[8].duty_cycle = self.speed
+        
+drive = DC4WD()    
+drive.back()
+drive.stop()
